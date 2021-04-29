b0c11b11bc3ebfa23e68d352118881f10a559f3e
Vinicius Lambert <99815703@ambev.com.br>
2021-04-29 20:39:40 -0300

    finish stl home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -16,11 +16,12 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
+            SizedBox(
+              height: 24,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -38,7 +39,21 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            QuizCardWidget()
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget()
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -9,21 +9,25 @@ class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
           border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
           color: AppColors.white,
           borderRadius: BorderRadius.circular(10)),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 40,
             width: 40,
             child: Image.asset(AppImages.blocks),
           ),
+          SizedBox(height: 20),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
           ),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(
