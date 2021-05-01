3661c5e027dcd7a90ba21e812f40b9b3872781bb
Vinicius Lambert <99815703@ambev.com.br>
2021-05-01 11:52:57 -0300

    add shared models

---
+++ lib/shared/models/questions_model.dart
@@ -0,0 +1,9 @@
+class QuestionModel {
+  final String title;
+  final List<AwnserModel> awnsers;
+
+  QuestionModel({required this.title, required this.awnsers})
+      : assert(
+          awnsers.length == 4,
+        );
+}
